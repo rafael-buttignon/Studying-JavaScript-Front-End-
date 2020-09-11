5761a899d7a18651875a53e47ae07ad20dbf3eb7
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-09-11 00:52:23 -0300

    Adicionando comportamentos

--- 2- JavaScript introdução a Orientação a Objetos/index.js
+++ 2- JavaScript introdução a Orientação a Objetos/index.js
@@ -1,29 +1,49 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
 
+class ContaCorrente{
+    // # saldo --> Campo privado que vai ser implementado
+    agencia;
+    saldo = 0;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return
+        }
+        this.saldo += valor;
+    }
+}
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
 
  cliente1.nome = "Ricardo";
  cliente1.cpf= 11122233309;
- cliente1.agencia = 1001;
- cliente1.saldo = 0;
  cliente1.rg = 1231233;
 
 
- cliente2.nome = "Ricardo";
+ cliente2.nome = "Alice";
  cliente2.cpf= 11122233309;
- cliente2.agencia = 1001;
- cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+
+contaCorrenteRicardo.agencia = 112323-2;
+contaCorrenteRicardo.saldo = 0;
 
 
+contaCorrenteRicardo.depositar(100);
+const valorSacado = contaCorrenteRicardo.sacar(100);
 
-console.log(cliente1);
-console.log(cliente2);
+console.log(valorSacado);
 
+console.log(contaCorrenteRicardo);
